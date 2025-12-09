4566f04b060225f6a9179f18bbb6c800d4774142
shura <screw_impact@inbox.ru>
2025-12-09 21:39:01 +0300

    homefork3

--- src/project.php
+++ src/project.php
@@ -5,5 +5,5 @@ $linuxArray[
    'kitty'=>2020
    'Debian'=>2003 
  ];
- echo ( $linuxArray as $row => $values ) {
+ foreach( $linuxArray as $row => $values ) {
 echo $row . PHP_EOL . $values. "<br>"}
