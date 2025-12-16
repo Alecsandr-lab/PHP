968e0d4264e96f9cd502996fe21c45d3c81fbabe
kazash <screw-impact@inbox.ru>
2025-12-16 18:59:41 +0300

    data

---
+++ src/hph.php
@@ -0,0 +1,68 @@
+<?php 
+/*$A=3;
+$b=4;
+echo $A+$b;
+
+$d=56;
+$g=70;
+echo $d+$g;
+
+$h=90;
+$p=1000;
+echo $p+$h;
+*/
+/*function chisloplus($A,$b) {
+    return $A+$b;
+}
+
+echo chisloplus(30,14);
+echo PHP_EOL;
+echo chisloplus(67,68);
+echo PHP_EOL;
+echo chisloplus(50,88);
+
+
+function minus($A,$b) {
+    return $A-$b;
+}
+
+echo minus(800,300);
+echo PHP_EOL;
+echo minus(6780,6800);
+echo PHP_EOL;
+echo minus(5000,8889);
+
+
+
+
+function multiplication($A,$b) {
+    return $A*$b;
+}
+
+echo multiplication(85,3);
+echo PHP_EOL;
+echo multiplication(60,56);
+echo PHP_EOL;
+echo multiplication(5000,8889);
+
+function delic($A,$b) {
+    if ($b != 0) {
+         return $A/$b;
+    }
+}
+
+echo delic(800,400);
+echo PHP_EOL;
+echo delic(6780,400);
+echo PHP_EOL;
+echo delic(5000,889);*/
+
+function data($name) {
+    return "goodbye ". $name;
+}
+
+echo data("piterrparrkerr
+")
+
+?>
+
