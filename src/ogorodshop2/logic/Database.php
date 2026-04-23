95caa84f5de5ecb4e03cfb714ff9ada613e367e6
kazash <screw_impact@inbox.ru>
2026-04-23 18:40:29 +0300

    update1

---
+++ src/ogorodshop2/logic/Database.php
@@ -0,0 +1,49 @@
+<?php
+class Database {
+
+    private $host = 'localhost';
+
+    private $db   = 'db';
+
+    private $user = 'myuser';
+
+    private $pass = 'mypassword';
+
+    private $charset = 'utf8mb4';
+
+    private $pdo;
+
+    public function getConnection() {
+
+        if ($this->pdo === null) {
+
+            $dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset";
+
+            $options = [
+
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+
+                PDO::ATTR_EMULATE_PREPARES   => false,
+
+            ];
+
+            try {
+
+                $this->pdo = new PDO($dsn, $this->user, $this->pass, $options);
+
+            } catch (PDOException $e) {
+
+                throw new PDOException($e->getMessage(), (int)$e->getCode());
+
+            }
+
+        }
+
+        return $this->pdo;
+
+    }
+
+}
+?>
--- src/ogorodshop2/logic/search.php
+++ src/ogorodshop2/logic/search.php
@@ -1,9 +1,27 @@
 <?php 
-class logic {
-
-
-
-
-
+require_once "Database.php";
+class Search {
+public $query;
+public $db;
+public function ___construct($query,$db) {
+    $this->query=$query;
+    $this->db=$db;
 }
+
+private function _search() {
+     $stmt=$this->db->prepare("SELECT * FROM LIKE `products` WHERE `name` LIKE %?%");
+    $stmt->execute([$this->qurey]);
+
+    return $stmt->fetchAll();
+}
+
+public function response() {
+    return json_encode($this-> _search());
+}
+}
+$db= new Database();
+$dbConnection= $db->getConnection();
+$search = new Search($_POST["query"],$dbConnection);
+ echo $search->response();
+
 ?>
